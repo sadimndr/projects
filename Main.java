4260c11e66fed830acbd7238c3ce0b04eea6a8d1
sadichhya manandhar <sadichhyamndr@gmail.com>
2021-05-01 09:32:31 +0545

    pass by vaue implementation in java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  static void myMethod(int x,int y) {
+      int sum=x+y;
+    System.out.println(sum);
+  }
+
+  public static void main(String[] args) {
+    int a=10,b=20;
+    myMethod(a,b);
+  }
+}
